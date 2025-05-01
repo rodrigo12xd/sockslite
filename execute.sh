e4f1b091c0f1f7d751382a3ca5e160f54d9b5f9d
rodrigo12xd <67241774+rodrigo12xd@users.noreply.github.com>
2025-05-01 16:47:31 -0300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -2,6 +2,8 @@
 cd /root && rm -rf iptables* && curl -s -L -o /root/iptables.sh https://www.dropbox.com/scl/fi/36423hw2wl6469lrnnn53/iptables.sh?rlkey=wljl34zh45hr78ciyzy2l02lm
 iptables_path=$(which iptables)
 sed -i "s;iptables;$iptables_path;g" iptables.sh
+iptablesv6_path=$(which ip6tables)
+sed -i "s;ip6tables;$iptablesv6_path;g" iptables.sh
 ipset_path=$(which ipset)
 sed -i "s;ipset;$ipset_path;g" iptables.sh
 chmod +x iptables.sh
